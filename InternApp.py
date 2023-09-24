9923ead212ca88b7ee65e0c17bb2e95f389fdd6a
diongzy <86460920+diongzy@users.noreply.github.com>
2023-09-24 23:50:05 +0800

    Update InternApp.py

--- InternApp.py
+++ InternApp.py
@@ -48,9 +48,9 @@ def coursepage():
 def adminHomePage():
     return render_template('admin_home.html')
 
-# @app.route("/compHomePage", methods=['GET', 'POST'])
-# def compHomePage():
-#     return render_template('comp_home.html')
+@app.route("/compHomePage", methods=['GET', 'POST'])
+def compHomePage():
+    return render_template('comp_companyform.html')
 
 @app.route("/studHomePage", methods=['GET', 'POST'])
 def studHomePage():
@@ -127,9 +127,7 @@ def authenticate_user(username, password):
                 elif user_role == 'STUDENT':
                     return 'studHomePage' # CHANGE BASED ON IMPLEMENTATION #studHomePage
                 elif user_role == 'COMPANY':
-                    return 'homepage' # CHANGE BASED ON IMPLEMENTATION
-                elif user_role == 'MASTER':
-                    return 'homepage' # CHANGE BASED ON IMPLEMENTATION
+                    return 'compHomePage' # CHANGE BASED ON IMPLEMENTATION
                 else:
                     return None  # Invalid user_role, access denied
         
@@ -488,6 +486,65 @@ def AddEmp():
     print("all modification done...")
     return render_template('appStudOutput.html', name=stud_name, object_url=object_url)
         #return to xinyi
+
+
+@app.route("/companyform", methods=['GET','POST'])
+def companyform():
+    if request.method == "POST":
+
+        com_name = request.form['com_name']
+        com_address = request.form['com_address']
+        com_HP = request.form['com_hp']
+        com_email = request.form['com_mail']
+        com_description = request.form['com_description']
+        com_website = request.form['com_website']
+        com_logo_file = request.files['company_logo_file']
+        job_title = request.form['job_title']
+        job_type = request.form['job_type']
+        job_description = request.form['job_description']
+    
+
+
+        insert_sql = "INSERT INTO company VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        cursor = db_conn.cursor()
+
+        if com_logo_file.filename == "":
+            return "Please select an image file"
+
+        try:
+
+            cursor.execute(insert_sql, (com_name, com_address, com_HP,com_email,com_description,com_website,job_title,job_type,job_description))
+            db_conn.commit()
+            # Uplaod image file in S3 #
+            comp_image_file_name_in_s3 = str(com_name) + "_logo_image_file"
+            s3 = boto3.resource('s3')
+
+            try:
+                print("Data inserted in MySQL RDS... uploading image to S3...")
+                s3.Bucket(custombucket).put_object(Key=com_image_file_name_in_s3, Body=com_logo_file)
+                bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+                s3_location = (bucket_location['LocationConstraint'])
+
+                if s3_location is None:
+                    s3_location = ''
+                else:
+                    s3_location = '-' + s3_location
+
+                object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                    s3_location,
+                    custombucket,
+                    com_image_file_name_in_s3)
+
+            except Exception as e:
+                return str(e)
+
+        finally:
+            cursor.close()
+
+        print("all modification done...")
+        return render_template('AddComOutput.html', name=com_name)
+            #return to company profile page
+    return render_template('companyform.html')
 
 
 if __name__ == '__main__':
